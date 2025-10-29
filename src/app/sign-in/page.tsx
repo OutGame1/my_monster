94298662f1e58eefeefaa566c72d62d6e7d30aee
Alexis LAMBERT <alexis.lambert600@gmail.com>
2025-10-29 23:38:50 +0100

    Redirection vers le dashboard en accédant aux pages de connexion si déjà connecté

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,14 +1,22 @@
 import type { ReactNode } from 'react'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import AuthPageLayout from '@/components/forms/AuthPageLayout'
 import SignInForm from '@/components/forms/SignInForm'
+import { getSession } from '@/lib/auth'
 
 /**
  * Page d'authentification pour la connexion des joueurs existants.
  *
  * @returns {ReactNode} Interface de connexion encapsulée dans le layout d'authentification.
  */
-export default function SignInPage (): ReactNode {
+export default async function SignInPage (): Promise<ReactNode> {
+  const session = await getSession()
+
+  if (session !== null) {
+    redirect('/app')
+  }
+
   return (
     <AuthPageLayout
       title='Bienvenue !'
