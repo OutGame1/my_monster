65e9616c151ef967e338db7efe25bb92da0376be
Alexis LAMBERT <alexis.lambert600@gmail.com>
2025-10-29 23:38:50 +0100

    Redirection vers le dashboard en accédant aux pages de connexion si déjà connecté

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,14 +1,22 @@
 import type { ReactNode } from 'react'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import AuthPageLayout from '@/components/forms/AuthPageLayout'
 import SignUpForm from '@/components/forms/SignUpForm'
+import { getSession } from '@/lib/auth'
 
 /**
  * Page d'inscription permettant de créer un nouveau compte My Monster.
  *
  * @returns {ReactNode} Interface de création de compte avec redirection vers la connexion.
  */
-export default function SignUpPage (): ReactNode {
+export default async function SignUpPage (): Promise<ReactNode> {
+  const session = await getSession()
+
+  if (session !== null) {
+    redirect('/app')
+  }
+
   return (
     <AuthPageLayout
       title="Rejoignez l'aventure !"
